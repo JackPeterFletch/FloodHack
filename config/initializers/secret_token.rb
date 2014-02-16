011314573d5f70c84004fab4a50eddcbf1081e47
Rob Cresswell <rcc21@kent.ac.uk>
2014-02-16 11:34:38 +0000

    Default Rails 4 env + error handling & thin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FloodHack::Application.config.secret_key_base = 'c482fe567414d5af274cf1d0572f4cefbdf3372318ea088b1ebaaf10a733e1a03d480f45b1c64ad0781dd1807096f7e374a23709eb3b482efc5fb01e41cef515'
