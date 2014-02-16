a7c3a775ba6149c75ffbb5b11a975f89d1ec853e
Rob Cresswell <rcc21@kent.ac.uk>
2014-02-16 14:47:37 +0000

    API for iOS Auth

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,15 @@
 FloodHack::Application.routes.draw do
-  devise_for :users
+	namespace :api do
+	  devise_for :users
+		resources :recipes, :only=>[:index, :show]
+	end  
+
+	devise_for :users
 
 	resources :users
 	resources :alerts
 
-  match 'alerttest' => 'alerts#send', :via => :get
+  match 'alerttest' => 'alerts#sendAlert', :via => :get
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
