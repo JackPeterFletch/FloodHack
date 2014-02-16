b288271b719d21fa99b880da68c6b68f1e0e9f43
Juan Garcia Alvite <juanga3@cisco.com>
2014-02-16 12:33:44 +0000

    Framework for mainactivity complete

--- androidApp/src/com/teamkent/ukfloodalerts/MainActivity.java
+++ androidApp/src/com/teamkent/ukfloodalerts/MainActivity.java
@@ -1,13 +1,19 @@
 package com.teamkent.ukfloodalerts;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationListener;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements
+LocationListener,
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,33 @@ public class MainActivity extends Activity {
 	 * @see android.app.Activity#onResume()
 	 */
 	@Override
+	protected void onStart(){
+		super.onStart();
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onResume()
+	 */
+	@Override
+	protected void onStop(){
+		super.onStop();
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onResume()
+	 */
+	@Override
+	protected void onPause(){
+		super.onPause();
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onResume()
+	 */
+	@Override
 	protected void onResume(){
 		//Check for Google Play Services, prompt for instal if not available.
 		final int RQS_GooglePlayServices = 1;
@@ -37,6 +70,30 @@ public class MainActivity extends Activity {
 			 GooglePlayServicesUtil.getErrorDialog(status, this, RQS_GooglePlayServices).show();
 		}
 		super.onResume();
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
