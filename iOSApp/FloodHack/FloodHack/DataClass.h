9d64452bfa8e1cc363c0caf98d34dfa747d4f928
John <jodavidg@cisco.com>
2014-02-16 16:33:25 +0000

    Added data storage to iOS app.

---
+++ iOSApp/FloodHack/FloodHack/DataClass.h
@@ -0,0 +1,26 @@
+//
+//  DataClass.h
+//  FloodHack
+//
+//  Created by John Davidge on 02/16/13.
+//  Copyright (c) 2014 John Davidge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataClass : NSObject {
+    NSString *user;
+    NSString *password;
+    NSString *url;
+    NSString *device_id;
+    NSString *filePath;
+    NSMutableDictionary *data_storage;
+}
+@property(nonatomic,retain)NSString *user;
+@property(nonatomic,retain)NSString *password;
+@property(nonatomic,retain)NSString *url;
+@property(nonatomic,retain)NSString *device_id;
+@property(nonatomic,retain)NSString *filePath;
+@property(nonatomic,retain)NSMutableDictionary *data_storage;
++(DataClass*)getInstance;
+@end
