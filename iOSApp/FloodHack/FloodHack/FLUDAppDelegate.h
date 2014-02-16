61742d92496f7aa039eb565aa78e26a446a53106
Jack Fletcher <jackfletcher2010@googlemail.com>
2014-02-16 11:51:01 +0000

    Added iOS project

---
+++ iOSApp/FloodHack/FloodHack/FLUDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FLUDAppDelegate.h
+//  FloodHack
+//
+//  Created by Jack Fletcher on 2/16/14.
+//  Copyright (c) 2014 TeamKent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLUDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
