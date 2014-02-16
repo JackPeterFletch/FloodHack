1c5e813cb2bb560a1a1e278b8d51cb70203202b0
Juan Garcia Alvite <juanga3@cisco.com>
2014-02-16 12:29:51 +0000

    Android app now tests for google play services and launches

--- androidApp/src/com/teamkent/ukfloodalerts/MainActivity.java
+++ androidApp/src/com/teamkent/ukfloodalerts/MainActivity.java
@@ -1,5 +1,8 @@
 package com.teamkent.ukfloodalerts;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -17,6 +20,23 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onResume()
+	 */
+	@Override
+	protected void onResume(){
+		//Check for Google Play Services, prompt for instal if not available.
+		final int RQS_GooglePlayServices = 1;
+		int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+		if(status == ConnectionResult.SUCCESS) {
+		    //We're good to continue - yay
+		}else{
+			 GooglePlayServicesUtil.getErrorDialog(status, this, RQS_GooglePlayServices).show();
+		}
+		super.onResume();
 	}
 
 }
