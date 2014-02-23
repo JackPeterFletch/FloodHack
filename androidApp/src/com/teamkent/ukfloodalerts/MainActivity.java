9125b1137c5a9ffedf5559f5e1cfd330459eeb73
Juan Garcia Alvite <juanga3@cisco.com>
2014-02-23 14:11:48 +0000

    Cleaned up a lot of GUI elements. more cleanup needed. This code is making me ill.

--- androidApp/src/com/teamkent/ukfloodalerts/MainActivity.java
+++ androidApp/src/com/teamkent/ukfloodalerts/MainActivity.java
@@ -69,13 +69,6 @@ GooglePlayServicesClient.OnConnectionFailedListener {
 
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 	/*
 	 * (non-Javadoc)
 	 * @see android.app.Activity#onResume()
